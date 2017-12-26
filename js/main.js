31bdf6123bde126b6a88cd5a75e851bf803d6029
haiyy <m18731668012_1@163.com>
2017-12-26 23:47:45 +0800

    tow

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const Alert=function(opt){
    this.tit=opt.title;
    this.btnbox=opt.btnbox;
+   this.light=opt.light?opt.light:false;
+   this.times=opt.times?opt.times:1000;
+   this.drag=opt.drag?opt.drag:false;
    this.init();
 }
 Alert.prototype={
@@ -22,12 +25,55 @@ Alert.prototype={
           button.innerHTML=this.btnbox[i];
        }
        document.body.appendChild(mark);
+       var timer=null;
+       if(this.light){
+            timer=setInterval(function(){
+            document.body.removeChild(mark);
+            clearInterval(timer);
+           },this.times);
+
+       };
        var btn=alertbox.querySelectorAll('button');
        btn[0].onclick=function(){
          document.body.removeChild(mark);
+         clearInterval(timer);
        }
        btn[1].onclick=function(){
          document.body.removeChild(mark);
+         clearInterval(timer);
+       }
+
+       if(this.drag){
+          var w=alertbox.offsetWidth;
+          var h=alertbox.offsetHeight;
+          var winw=document.documentElement.clientWidth||document.body.clientWidth;
+          var winh=document.documentElement.clientHeight||document.body.clientHeight;
+          alertbox.onmousedown=function(e){
+            var ev=e||event;
+            var x=ev.clientX-this.offsetLeft;
+            var y=ev.clientY-this.offsetTop;
+            document.onmousemove=function(e){
+              var ev=e||event;
+              var pagex=ev.clientX-x;
+              var pagey=ev.clientY-y;
+              if(pagex<=0){
+                pagex=0;
+              }else if(pagex>=winw-w){
+                pagex=winw-w;
+              }
+              if(pagey<=0){
+                pagey=0;
+              }else if(pagey>=winh-h){
+                pagey=winh-h;
+              }
+              alertbox.style.left=pagex+200+'px';
+              alertbox.style.top=pagey+100+'px';
+            }
+            document.onmouseup=function(){
+              document.onmousemove=null;
+              document.onmouseup=null;
+            }
+          }
        }
     }
 }
